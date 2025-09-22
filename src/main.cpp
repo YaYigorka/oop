8e33bce267004d7e4e2e6708f811d4a75f5f1d9a
Egor <g.v.strokov@yande.ru>
2025-09-22 18:36:46 +0300

    bugfix

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "../include/removingVowels.h"
+#include "removingVowels.h"
 
 
 int main(int argc, char * argv[]) {
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../include/removingVowels.h"
+#include "removingVowels.h"
 #include <string>
 
 
